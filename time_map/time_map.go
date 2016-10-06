e15fad55cb5d0d77a498eb5346a373baf2d5c904
Sándor Lipcsei <sandor.lipcsei@gmail.com>
2016-10-06 16:14:01 +0200

    created some tests

--- time_map/time_map.go
+++ time_map/time_map.go
@@ -2,17 +2,17 @@ package timemap
 
 import hammingweight "github.com/lipcsei/scoring/hammingWeight"
 
-type TimeMap [4]uint32
+type TimeMap [4]int
 
 func New(starter bool) TimeMap {
 	if starter {
-		return TimeMap{}
+		return TimeMap{-1, -1, -1, -1}
 	}
 	return TimeMap{0, 0, 0, 0}
 }
 
-func (t TimeMap) Count() uint32 {
-	var sum, lastWordError uint32
+func (t TimeMap) Count() int {
+	var sum, lastWordError int
 	sum = 0
 	for _, i := range t {
 		sum += hammingweight.Of(i)
@@ -28,7 +28,7 @@ func (t TimeMap) Not() TimeMap {
 	return t
 }
 
-func bitwiseNot(b uint32) uint32 {
+func bitwiseNot(b int) int {
 	return 0xFFFFFFFF ^ b
 }
 
@@ -46,40 +46,54 @@ func (t TimeMap) Or(other TimeMap) TimeMap {
 	return t
 }
 
-func (t TimeMap) Get(minute uint32) bool {
+func (t TimeMap) Get(minute int) bool {
 	if minute <= 0 || minute > 120 {
 		return false
 	}
-
 	return queryBit(t[getFieldIndex(minute)], minute)
 }
 
-func queryBit(value uint32, minute uint32) bool {
-	mask := 1 << ((minute - 1) % 32)
-	return (value & uint32(mask)) != 0
+func queryBit(value int, minute int) bool {
+	mask := 1 << getOffset(minute)
+	return (value & int(mask)) != 0
 }
 
-func getBitmask(minute uint32) uint32 {
-	offset := (minute - 1) % 32
-	return (1 << offset) - 1
+func getBitmask(minute int) int {
+	return int((1 << getOffset(minute)) - 1)
 }
 
-func (t TimeMap) SetFrom(minute uint32) {
+func getOffset(minute int) uint {
+	return uint((minute - 1) % 32)
+}
+
+func (t TimeMap) SetFrom(minute int) TimeMap {
+
 	fieldIndex := getFieldIndex(minute)
-	for i := uint32(len(t) - 1); i > fieldIndex; i-- {
-		t[i] = 0
+	for i := len(t) - 1; i > fieldIndex; i-- {
+		t[i] = -1
 	}
 	t[fieldIndex] = t[fieldIndex] | bitwiseNot(getBitmask(minute))
+	return t
 }
 
-func (t TimeMap) UnsetFrom(minute uint32) {
+func (t TimeMap) UnsetFrom(minute int) TimeMap {
+
 	fieldIndex := getFieldIndex(minute)
-	for i := uint32(len(t) - 1); i > fieldIndex; i-- {
+	for i := len(t) - 1; i > fieldIndex; i-- {
 		t[i] = 0
 	}
 	t[fieldIndex] = t[fieldIndex] & getBitmask(minute)
+	return t
 }
 
-func getFieldIndex(minute uint32) uint32 {
+func getFieldIndex(minute int) int {
 	return (minute - 1) / 32
+}
+
+func Between(from int, to int) TimeMap {
+	return TimeMap{0, 0, 0, 0}.SetFrom(from).UnsetFrom(to + 1)
+}
+
+func Until(minute int) TimeMap {
+	return TimeMap{-1, -1, -1, -1}.UnsetFrom(minute + 1)
 }
--- time_map/time_map_test.go
+++ time_map/time_map_test.go
@@ -1,7 +1,6 @@
 package timemap
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,23 +8,13 @@ import (
 
 func TestNewMethod(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal(TimeMap{}, New(true))
+	assert.Equal(TimeMap{-1, -1, -1, -1}, New(true))
 	assert.Equal(TimeMap{0, 0, 0, 0}, New(false))
-}
-
-func TestBitCountZero(t *testing.T) {
-	assert := assert.New(t)
-	assert.Equal(uint32(0), New(false).Count())
-}
-
-func TestBitCountNormal(t *testing.T) {
-	assert := assert.New(t)
-	assert.Equal(uint32(16), TimeMap{15, 15, 15, 15}.Count())
 }
 
 func TestBitwiseNotFunction(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal(uint32(0xFFF0FFF0), bitwiseNot(0xF000F))
+	assert.Equal(0xFFF0FFF0, bitwiseNot(0xF000F))
 }
 
 func TestNotMethod(t *testing.T) {
@@ -52,32 +41,180 @@ func TestOrMethod(t *testing.T) {
 	assert.Equal(expected, a.Or(b))
 }
 
-func TestGetMethod(t *testing.T) {
-	assert := assert.New(t)
-	assert.False(TimeMap{}.Get(0))
-	assert.False(TimeMap{}.Get(121))
-
-	fmt.Println(49 % 32)
-
-}
-
 func TestGetBitmaskFunction(t *testing.T) {
 	assert := assert.New(t)
-	expected := uint32(0x1FFFF)
-	assert.Equal(expected, getBitmask(82))
+	assert.Equal(0x1FFFF, getBitmask(82))
 }
 
 func TestGetFieldIndexFunction(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal(uint32(0), getFieldIndex(12))
-	assert.Equal(uint32(1), getFieldIndex(40))
-	assert.Equal(uint32(2), getFieldIndex(70))
-	assert.Equal(uint32(3), getFieldIndex(100))
+	assert.Equal(0, getFieldIndex(12))
+	assert.Equal(1, getFieldIndex(40))
+	assert.Equal(2, getFieldIndex(70))
+	assert.Equal(3, getFieldIndex(100))
 }
 
-// func TestQueryBitFunction(t *testing.T) {
-// 	assert := assert.New(t)
-// 	// assert.True(queryBit(0, 42))
-// }
+func TestGetOffsetMethod(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(uint(17), getOffset(82))
+	assert.Equal(uint(28), getOffset(29))
+}
 
-// func TestSet
+func TestQueryBitFunction(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(queryBit(-1, 29))
+	assert.True(queryBit(67108863, 68))
+}
+
+func TestStarterReturnsTrueForEveryMinute(t *testing.T) {
+	assert := assert.New(t)
+
+	timemap := New(true)
+	for i := 1; i < 120; i++ {
+		assert.True(timemap.Get(i))
+	}
+}
+
+func TestNotStarterReturnsFalseForEveryMinute(t *testing.T) {
+	assert := assert.New(t)
+
+	timemap := New(false)
+	for i := 1; i < 120; i++ {
+		assert.False(timemap.Get(i))
+	}
+}
+func TestGetReturnsFalseForInvalidValues(t *testing.T) {
+	assert := assert.New(t)
+	timemap := New(true)
+	assert.False(timemap.Get(0))
+	assert.False(timemap.Get(125))
+}
+
+func TestGetReturnsCorrectValues(t *testing.T) {
+	assert := assert.New(t)
+	timemap := TimeMap{15, 0, 15, 0}
+
+	for i := 1; i <= 4; i++ {
+		assert.True(timemap.Get(i))
+	}
+	for i := 5; i <= 64; i++ {
+		assert.False(timemap.Get(i))
+	}
+	for i := 65; i <= 68; i++ {
+		assert.True(timemap.Get(i))
+	}
+	for i := 69; i <= 120; i++ {
+		assert.False(timemap.Get(i))
+	}
+}
+
+func TestSetThenGetLower(t *testing.T) {
+	assert := assert.New(t)
+	timemap := TimeMap{0, 0, 0, 0}
+	timemap = timemap.SetFrom(10)
+	for i := 1; i <= 9; i++ {
+		assert.False(timemap.Get(i))
+	}
+
+	for i := 10; i <= 120; i++ {
+		assert.True(timemap.Get(i))
+	}
+}
+
+func TestUnsetThenGetLower(t *testing.T) {
+	assert := assert.New(t)
+	timemap := TimeMap{-1, -1, -1, -1}
+	timemap = timemap.UnsetFrom(10)
+	for i := 1; i <= 9; i++ {
+		assert.True(timemap.Get(i))
+	}
+
+	for i := 10; i <= 120; i++ {
+		assert.False(timemap.Get(i))
+	}
+}
+
+func TestUnsetThenGetUpper(t *testing.T) {
+	assert := assert.New(t)
+	timemap := TimeMap{-1, -1, -1, -1}
+	timemap = timemap.UnsetFrom(75)
+	for i := 1; i <= 74; i++ {
+		assert.True(timemap.Get(i))
+	}
+
+	for i := 75; i <= 120; i++ {
+		assert.False(timemap.Get(i))
+	}
+}
+
+func TestBitCountZero(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(0, New(false).Count())
+}
+
+func TestBitCountNormal(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(16, TimeMap{15, 15, 15, 15}.Count())
+}
+
+func TestBitCountOverflowStarter(t *testing.T) {
+	assert := assert.New(t)
+	timemap := New(true)
+	assert.Equal(120, timemap.Count())
+}
+
+func TestBitCountOverflowAfterSet(t *testing.T) {
+	assert := assert.New(t)
+	timemap := TimeMap{0, 0, 0, 0}
+	timemap = timemap.SetFrom(71)
+	assert.Equal(50, timemap.Count())
+}
+
+func TestUntilLower(t *testing.T) {
+	assert := assert.New(t)
+	timemap := Until(25)
+	for i := 1; i <= 25; i++ {
+		assert.True(timemap.Get(i))
+	}
+
+	for i := 26; i <= 120; i++ {
+		assert.False(timemap.Get(i))
+	}
+}
+
+func TestUntilHigher(t *testing.T) {
+	assert := assert.New(t)
+	timemap := Until(75)
+	for i := 1; i <= 75; i++ {
+		assert.True(timemap.Get(i))
+	}
+
+	for i := 76; i <= 120; i++ {
+		assert.False(timemap.Get(i))
+	}
+}
+
+func TestUntilMax(t *testing.T) {
+	assert := assert.New(t)
+	timemap := Until(120)
+	for i := 1; i <= 120; i++ {
+		assert.True(timemap.Get(i))
+	}
+}
+
+func TestBetweenLower(t *testing.T) {
+	assert := assert.New(t)
+	timemap := Between(25, 45)
+	for i := 1; i < 25; i++ {
+		assert.False(timemap.Get(i))
+	}
+
+	for i := 25; i <= 45; i++ {
+		assert.True(timemap.Get(i))
+	}
+
+	for i := 46; i <= 120; i++ {
+		assert.False(timemap.Get(i))
+	}
+
+}
